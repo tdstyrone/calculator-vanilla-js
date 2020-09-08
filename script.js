b163116e8c962c933d606fa41e5dce047c914082
Tyrone Smith <tdstyrone@gmail.com>
2020-09-08 16:48:19 -0400

    Fixed division error

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ class Calculator{
       case '*':
         computation = prev * current;
         break;
-      case '/':
+      case '÷':
         computation = prev / current;
         break;
       default:
